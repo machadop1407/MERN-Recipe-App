4e2a15d9ab6a4d5df9da2ccd468abc68a3412247
Pedro Henrique Machado <machadop1407@gmail.com>
2023-03-07 11:46:56 -0800

    Update Users.js

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, requirrd: true, unique: true },
-  password: { type: String, requirrd: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
