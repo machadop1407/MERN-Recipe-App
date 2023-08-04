886618c37bf124f1569fc76d369858cbeb819b53
kevinxinming <86008249+kevinxinming@users.noreply.github.com>
2023-08-04 12:32:45 -0400

    Server changing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Server from "./Server";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <App />
+    <Server />
 );
