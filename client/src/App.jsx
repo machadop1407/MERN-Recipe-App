8e678b4aec2086f07466156ad952286ac998649f
kevinxinming <86008249+kevinxinming@users.noreply.github.com>
2023-08-04 12:02:17 -0400

    Sass

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./style.scss";
+import "./scss/style.scss";
 import { Navbar } from "./components/navbar";
 import { Auth } from "./pages/auth";
 import { CreateRecipe } from "./pages/create-recipe";
